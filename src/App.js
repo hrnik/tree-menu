1903e1c7bb7afbc22b89c332f88643a63b90c0de
Nikita Khristinin <nkhristinin@gmail.com>
2021-02-25 13:47:46 +0100

    Add some layout components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+import { Header, SideBar, Container, Content, Main } from "./components/Layout";
+const GlobalStyle = createGlobalStyle`
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smsoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <GlobalStyle />
+
+      <Header />
+      <Main>
+        <SideBar></SideBar>
+        <Content></Content>
+        <div />
+      </Main>
+    </Container>
   );
 }
 
---
+++ src/components/Layout.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background: black;
+`;
+
+export const Container = styled.div`
+  height: 100vh;
+  min-height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 50px 1fr;
+`;
+
+export const Main = styled.div`
+  display: grid;
+  grid-template-columns: 400px 1fr;
+  grid-template-rows: 1fr;
+  overflow: hidden;
+`;
+
+export const Content = styled.div`
+  padding: 20px;
+  overflow: scroll;
+`;
+
+export const SideBar = styled.aside`
+  background: lightblue;
+  overflow: scroll;
+`;
