6a05dd2eb00338dd8581d536059264d209bd5cf1
Nikita Khristinin <nkhristinin@gmail.com>
2021-03-01 11:32:57 +0100

    Add animation

--- src/Tree/components/TreeItem.js
+++ src/Tree/components/TreeItem.js
@@ -1,6 +1,9 @@
 import { Component } from "react";
 import isEqual from "react-fast-compare";
+import { useSpring, animated } from "react-spring";
+import useResizeObserver from "use-resize-observer";
 import { TreeItemRow, TreeItemLink } from "./Styled";
+
 import ArrowIcon from "./ArrowIcon";
 
 class TreeItem extends Component {
@@ -9,54 +12,75 @@ class TreeItem extends Component {
   }
 
   render() {
-    const { item, onToggle, onSelect, level, isAnchor } = this.props;
-    return (
-      <>
-        <TreeItemRow
-          isSelected={item.isSelected || isAnchor}
-          isAnchor={isAnchor}
-          level={level}
-          onClick={() => (item.pages ? onToggle(item) : onSelect(item))}
-          data-test-hook="tree-item"
-        >
-          <TreeItemLink href="#" isSelected={item.isSelected}>
-            {item.pages &&
-              (item.isOpen ? (
-                <ArrowIcon isDown={false} />
-              ) : (
-                <ArrowIcon isDown={true} />
-              ))}
-            {item.title}
-          </TreeItemLink>
-        </TreeItemRow>
-
-        {item.anchors &&
-          item.isSelected &&
-          item.anchors.map((children) => (
-            <TreeItem
-              item={children}
-              onToggle={onToggle}
-              onSelect={onSelect}
-              key={children.id}
-              level={level}
-              isAnchor={true}
-            />
-          ))}
-
-        {item.isOpen &&
-          item.pages &&
-          item.pages.map((children) => (
-            <TreeItem
-              item={children}
-              onToggle={onToggle}
-              onSelect={onSelect}
-              key={children.id}
-              level={level + 1}
-            />
-          ))}
-      </>
-    );
+    return <TreeItemView {...this.props} />;
   }
 }
+
+const TreeItemView = ({ item, onToggle, onSelect, level, isAnchor }) => {
+  const { ref, height = 0 } = useResizeObserver();
+
+  const props = useSpring({
+    height: item.isOpen ? height : 0,
+  });
+
+  return (
+    <>
+      <TreeItemRow
+        isSelected={item.isSelected || isAnchor}
+        isAnchor={isAnchor}
+        level={level}
+        onClick={() => (item.pages ? onToggle(item) : onSelect(item))}
+        data-test-hook="tree-item"
+      >
+        <TreeItemLink href="#" isSelected={item.isSelected}>
+          {item.pages &&
+            (item.isOpen ? (
+              <ArrowIcon isDown={false} />
+            ) : (
+              <ArrowIcon isDown={true} />
+            ))}
+          {item.title}
+        </TreeItemLink>
+      </TreeItemRow>
+
+      <animated.div
+        style={{
+          ...props,
+          overflow: "hidden",
+          position: "relative",
+          willChange: "auto",
+        }}
+        className="menu"
+      >
+        <div ref={ref}>
+          {item.anchors &&
+            item.isSelected &&
+            item.anchors.map((children) => (
+              <TreeItem
+                item={children}
+                onToggle={onToggle}
+                onSelect={onSelect}
+                key={children.id}
+                level={level}
+                isAnchor={true}
+              />
+            ))}
+
+          {item.isOpen &&
+            item.pages &&
+            item.pages.map((children) => (
+              <TreeItem
+                item={children}
+                onToggle={onToggle}
+                onSelect={onSelect}
+                key={children.id}
+                level={level + 1}
+              />
+            ))}
+        </div>
+      </animated.div>
+    </>
+  );
+};
 
 export default TreeItem;
